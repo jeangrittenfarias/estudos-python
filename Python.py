f0fe6005b4efbe307a3d1ec606a167c8696576b6
Jean Farias <jeanfariaspinheiro@gmail.com>
2026-05-27 23:43:05 -0300

    Commit inicial

---
+++ Python.py
@@ -0,0 +1,49 @@
+# Hello World
+
+# print("hello world")
+
+
+# =========================
+# Calculadora simples
+# =========================
+
+# num1 = input("Digite o primeiro número: ")
+# num2 = input("Digite o segundo número: ")
+# print("A soma é: " + num1 + num2) 
+
+
+# =========================
+# Verificador de idade
+# =========================
+
+# idade = int(input("Digite sua idade: "))
+# if idade >=18:
+#     print("Acesso liberado")
+# else:
+#     print("Acesso negado")
+
+
+# =========================
+# Sistema de senha
+# =========================
+
+# senha = "pepino"
+# tentativa_senha = input("Digite sua senha: ")
+# if senha == tentativa_senha:
+#     print("Acesso Liberado!")
+# else:
+#     print("Errou...")
+
+
+# =========================
+# Sistema de notas
+# =========================
+
+# nota = float(input("Digite sua nota: "))
+# if nota >=7:
+#     print("Aprovado")
+# elif nota >=5:
+#     print("Recuperação")
+# else:
+#     print("Reprovado")
+
