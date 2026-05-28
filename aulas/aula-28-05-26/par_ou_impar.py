151e7c39f35790ee704b91d2a8918cd07c34d078
Jean Gritten <jeanfariaspinheiro@gmail.com>
2026-05-28 16:17:31 -0300

    exercicio par ou impar
    fiz mais alguns exercicios de um codigo que verifica se um numero é par ou impar

---
+++ aulas/aula-28-05-26/par_ou_impar.py
@@ -0,0 +1,37 @@
+# =========================
+# Par ou Ímpar
+# =========================
+
+
+# num = int(input("Digite um número: "))
+
+# calculo = (num%2)
+
+# par = int(0)
+# impar = int(1)
+
+# if calculo == par:
+#     print("Número par")
+# else:
+#     print("Número impar")
+
+
+# ==================VERSÃO-MAIS-SIMPLES===========================
+
+# num = int(input("Digite um número: "))
+
+# calculo = num % 2
+
+# if calculo == 0:
+#     print("Número par")
+# else:
+#     print("Número ímpar")
+
+# ==================VERSÃO-MAIS-SIMPLES-AINDA===========================
+
+num = int(input("Digite um número: "))
+
+if num % 2 == 0:
+    print("Número par")
+else:
+    print("Número ímpar")
