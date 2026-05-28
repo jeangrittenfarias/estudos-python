cfe64719d010491ec4d025b04e53efb40103e6b9
Jean Gritten <jeanfariaspinheiro@gmail.com>
2026-05-28 10:33:23 -0300

    Adicionei exercício anterior
    adicionei um exercício feito anteriormente no repositório para organização.

---
+++ estudos-python-master/estudos-python-master/aulas/aula-1/calculadora.py
@@ -0,0 +1,18 @@
+
+# =========================
+# Calculadora simples
+# =========================
+
+num1 = input("Digite o primeiro número: ")
+num2 = input("Digite o segundo número: ")
+print("A soma é: " + num1 + num2) 
+
+
+
+
+
+
+
+
+
+
