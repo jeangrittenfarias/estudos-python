7f70c8c841d2c30074665f27e0a65ef43c9e863f
Jean Farias <jeanfariaspinheiro@gmail.com>
2026-05-28 00:31:53 -0300

    melhoria na calculadora adicionando: subtração, divisão e multiplicação

--- aulas/aula-1/calculadora.py
+++ aulas/aula-1/calculadora.py
@@ -3,9 +3,16 @@
 # Calculadora simples
 # =========================
 
-num1 = input("Digite o primeiro número: ")
-num2 = input("Digite o segundo número: ")
-print("A soma é: " + num1 + num2) 
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+soma = num1 + num2
+subtracao = num1 - num2
+divisao = num1 / num2
+multiplicacao = num1 * num2
+print(f"Soma: {soma}")
+print(f"Subtração: {subtracao}")
+print(f"Divisão: {divisao}")
+print(f"Multiplicação: {multiplicacao}")
 
 
 
