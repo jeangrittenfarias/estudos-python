7cd245b000cc0dc758c994f6b632af8c0a0f0c74
Jean Gritten <jeanfariaspinheiro@gmail.com>
2026-05-30 15:55:28 -0300

    Coleções
    Comecei a ver coleções

---
+++ aulas/aula-30-05-26/colecoes.py
@@ -0,0 +1,40 @@
+# =========================
+# Maior Menor ou Igual
+# =========================
+
+
+# # coleção = variável com vários valores:
+
+# frutas = ["Banana", "Maça", "uva"]
+
+# # Troca um valor por outo, nesse caso maça por abacaxi
+
+# frutas[1] = "Abacaxi"
+
+# # Adiciona um valor no final
+
+# frutas.append("Morango")
+
+# # Adiciona um valor em um local especifico
+
+# frutas.insert(1, "Kiwi")
+
+# # Remove um valor da coleção
+
+# frutas.remove("Abacaxi")
+
+# if "uva" in frutas:
+#     print("Tem uva nas frutas!")
+
+# print(frutas)
+
+
+# minhas_tuplas = [("Maças", "Bananas"), ("Verde", "Vermelho")]
+# print(minhas_tuplas[1][0])
+
+# Dicionários
+
+dados = {"nome":"Jean", "idade":22}
+dados["idade"] = 19
+print(dados)
+
